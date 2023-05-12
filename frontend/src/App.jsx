136e316b5a1771f0cce5c3e146dfbaf7f97bf818
layconjohn <layconjohn@gmail.com>
2023-05-11 22:06:22 -0300

    feat: setAvatar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route  } from "react-router-dom";
 import Chat from "./pages/Chat";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import SetAvatar from "./pages/SetAvatar";
 
 export default function App() {
   return(
@@ -10,6 +11,7 @@ export default function App() {
       <Routes>
         <Route path="/sign-up" element={<SignUp />} />
         <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/set-avatar" element={<SetAvatar />} />
         <Route path="/" element={<Chat />} />
       </Routes>
 
---
+++ frontend/src/pages/SetAvatar.jsx
@@ -0,0 +1,134 @@
+import React, { useState, useCallback, useEffect } from "react";
+import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
+import axios from "axios";
+import loader from "../assets/loader.gif";
+import { setAvatarRouter } from "../utils/APIRoutes";
+import { Buffer } from "buffer";
+
+export default function SetAvatar() {
+    const api = `https://api.multiavatar.com`;
+    const navigate = useNavigate();
+    const toastOptions = {
+        position: "bottom-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+    };
+
+    const [avatars, setAvatars] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [selectedAvatar, setSelectedAvatar] = useState(undefined);
+
+    async function setProfilePicture() {
+        if(selectedAvatar===undefined) {
+            toast.error("Por favor selecione um avatar")
+        }
+    }
+
+    useEffect(() => {
+        async function fetchData() {
+            const data = [];
+            for(let i = 0; i<4;i++) {
+                const images = ["7dfcbb86a8404459a6", "f9df3b008a8fea8af4", "dcca6052cc824552d9", "0caa4cbffd2694e651"]
+                //const image = await axios.get(`${api}/${Math.round(Math.random() * 1000)}`);
+                const image = await axios.get(`${api}/${images[i]}`);
+                const buffer = new Buffer(image.data);
+                data.push(buffer.toString("base64"));
+            }
+            setAvatars(data);
+            setIsLoading(false);
+        }
+        fetchData();
+    }, []);
+
+    return (
+        <>
+           <Container>
+                <div className="title-container">
+                    <h1>Escolha um avatar para a seu perfil</h1>
+                </div>
+                <div className="avatars">
+                    {avatars.map((avatar, i) => {
+                        return (
+                            <div className={`avatar ${selectedAvatar === i ? "selected" : ""}`}  key={i}>
+                                <img src={`data:image/svg+xml;base64,${avatar}`} alt="avatar" 
+                                    onClick={() => setSelectedAvatar(i)}
+                                />
+                            </div>
+                        )
+                    })}
+                </div>
+                <button className="submit-button" onClick={setProfilePicture}>Escolher avatar</button>
+           </Container>
+           <ToastContainer />
+        </>
+    )
+}
+
+const Container = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    gap: 3rem;
+    background-color: #131324; 
+    height: 100vh;
+    width: 100vw;
+
+    .loader{
+        max-inline-size: 100%;
+
+    }
+
+    .title-container{
+        h1{
+            color: white;
+        }
+    }
+
+    .avatars{
+        display: flex;
+        gap: 2rem;
+        .avatar{
+            border: 0.4rem solid transparent;
+            padding: 0.4rem;
+            border-radius: 5rem;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            transition: 0.5s ease-in-out;
+            img{
+                height:6rem;
+            }
+        }
+        .selected{
+            border: 0.4rem solid #4e0eff;
+        }
+    }
+
+    .submit-button{
+        background-color: #997af0;
+        color: white;
+        padding: 1rem 2rem;
+        border: none;
+        font-weight: bold;
+        cursor: pointer;
+        border-radius: 0.4rem;
+        font-size: 1rem;
+        text-transform: uppercase;
+        transition: 0.5s ease-in-out;
+        &:hover {
+            background-color: #4e0eff;
+        }
+        &:active{
+            scale: 1.1;
+        }
+    }
+`;
