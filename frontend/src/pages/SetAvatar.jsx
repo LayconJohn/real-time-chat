422cf2ebcbcc11adec111e2070f8566e0e3d5179
layconjohn <layconjohn@gmail.com>
2023-05-12 00:32:35 -0300

    feat: use multiavatar api and function set profile avatar

--- frontend/src/pages/SetAvatar.jsx
+++ frontend/src/pages/SetAvatar.jsx
@@ -28,7 +28,21 @@ export default function SetAvatar() {
 
     async function setProfilePicture() {
         if(selectedAvatar===undefined) {
-            toast.error("Por favor selecione um avatar")
+            toast.error("Por favor selecione um avatar");
+        } else {
+            const user = await JSON.parse(localStorage.getItem("chat-app-user"));
+            const { data } = await axios.post(`${setAvatarRouter}/${user._id}`,{
+                image: avatars[selectedAvatar],
+            });
+
+            if(data.isSet) {
+                user.isAvatarImageSet = true;
+                user.avatarImage = data.image;
+                localStorage.setItem("chat-app-user", JSON.stringify(user));
+                navigate("/");
+            } else {
+                toast.error("Erro ao selecionar o avatar. Por favor, tente novamente", toastOptions);
+            }
         }
     }
 
@@ -36,9 +50,9 @@ export default function SetAvatar() {
         async function fetchData() {
             const data = [];
             for(let i = 0; i<4;i++) {
-                const images = ["7dfcbb86a8404459a6", "f9df3b008a8fea8af4", "dcca6052cc824552d9", "0caa4cbffd2694e651"]
-                //const image = await axios.get(`${api}/${Math.round(Math.random() * 1000)}`);
-                const image = await axios.get(`${api}/${images[i]}`);
+                //const images = ["7dfcbb86a8404459a6", "f9df3b008a8fea8af4", "dcca6052cc824552d9", "0caa4cbffd2694e651"]
+                const image = await axios.get(`${api}/${Math.round(Math.random() * 1000)}`);
+                //const image = await axios.get(`${api}/${images[i]}`);
                 const buffer = new Buffer(image.data);
                 data.push(buffer.toString("base64"));
             }
@@ -50,23 +64,32 @@ export default function SetAvatar() {
 
     return (
         <>
-           <Container>
-                <div className="title-container">
-                    <h1>Escolha um avatar para a seu perfil</h1>
-                </div>
-                <div className="avatars">
-                    {avatars.map((avatar, i) => {
-                        return (
-                            <div className={`avatar ${selectedAvatar === i ? "selected" : ""}`}  key={i}>
-                                <img src={`data:image/svg+xml;base64,${avatar}`} alt="avatar" 
-                                    onClick={() => setSelectedAvatar(i)}
-                                />
-                            </div>
-                        )
-                    })}
-                </div>
-                <button className="submit-button" onClick={setProfilePicture}>Escolher avatar</button>
-           </Container>
+            {
+                isLoading 
+                ? 
+                <Container>
+                    <img src={loader} alt="loader" className="loader"/>
+                </Container>
+                :
+                <Container>
+                    <div className="title-container">
+                        <h1>Escolha um avatar para a seu perfil</h1>
+                    </div>
+                    <div className="avatars">
+                        {avatars.map((avatar, i) => {
+                            return (
+                                <div className={`avatar ${selectedAvatar === i ? "selected" : ""}`}  key={i}>
+                                    <img src={`data:image/svg+xml;base64,${avatar}`} alt="avatar" 
+                                        onClick={() => setSelectedAvatar(i)}
+                                    />
+                                </div>
+                            )
+                        })}
+                    </div>
+                    <button className="submit-button" onClick={setProfilePicture}>Escolher avatar</button>
+                </Container>
+            }
+
            <ToastContainer />
         </>
     )
