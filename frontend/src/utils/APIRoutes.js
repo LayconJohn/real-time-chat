3d45c196d7ac7ffebdb60132c55b0b342e9a6874
layconjohn <layconjohn@gmail.com>
2023-05-13 23:38:15 -0300

    style: create container chat

--- frontend/src/utils/APIRoutes.js
+++ frontend/src/utils/APIRoutes.js
@@ -2,3 +2,4 @@ const host = "http://localhost:5000";
 export const signUpRouter = `${host}/api/auth/sign-up`; 
 export const signInRouter = `${host}/api/auth/sign-in`; 
 export const setAvatarRouter = `${host}/api/auth/set-avatar`;
+export const allUsersRouter = `${host}/api/auth/all-users`;
